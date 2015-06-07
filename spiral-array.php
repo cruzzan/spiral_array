b7f6760fb671de085d714b18a9da20e631a20bf0
Sebastian Hultstrand <sebastian@storm-net.se>
2015-06-07 03:31:12 +0200

    Added some comments to the code.

--- spiral-array.php
+++ spiral-array.php
@@ -1,14 +1,15 @@
 <?PHP
-$sideLength = 9;
+$sideLength = 9; // Edit this to change the number of elements per side.
 $inputArr = array();
 $outputArray = array(array());
 
-for($i = 0; $i < $sideLength * $sideLength; $i++){
+for($i = 0; $i < $sideLength * $sideLength; $i++){ // Edit the starting value of $i and the comparitor to toggle between starting at 1 or 0
 	array_push($inputArr, $i);
 }
 
 makeTopMost($inputArr, 0, $sideLength - 1, $sideLength - 1, 0, $outputArray);
 
+# Addes elements to the top most side of the 2d array that has not yeat been filled
 function makeTopMost($inputArr, $cT, $cB, $cR, $cL, &$outputArray){
 	if(sizeof($inputArr) != 0){
 		//echo "top ";
@@ -19,6 +20,7 @@ function makeTopMost($inputArr, $cT, $cB, $cR, $cL, &$outputArray){
 		makeRightMost($inputArr, $cT, $cB, $cR, $cL, $outputArray);
 	}
 }
+# Addes elements to the right most side of the 2d array that has not yeat been filled
 function makeRightMost($inputArr, $cT, $cB, $cR, $cL, &$outputArray){
 	if(sizeof($inputArr) != 0){
 		//echo "right ";
@@ -29,6 +31,7 @@ function makeRightMost($inputArr, $cT, $cB, $cR, $cL, &$outputArray){
 		makeBottomMost($inputArr, $cT, $cB, $cR, $cL, $outputArray);
 	}
 }
+# Addes elements to the bottom most side of the 2d array that has not yeat been filled
 function makeBottomMost($inputArr, $cT, $cB, $cR, $cL, &$outputArray){
 	if(sizeof($inputArr) != 0){
 		//echo "bottom ";
@@ -39,6 +42,7 @@ function makeBottomMost($inputArr, $cT, $cB, $cR, $cL, &$outputArray){
 		makeLeftMost($inputArr, $cT, $cB, $cR, $cL, $outputArray);
 	}
 }
+# Addes elements to the left most side of the 2d array that has not yeat been filled
 function makeLeftMost($inputArr, $cT, $cB, $cR, $cL, &$outputArray){
 	if(sizeof($inputArr) != 0){
 		//echo "left ";
@@ -53,7 +57,7 @@ function makeLeftMost($inputArr, $cT, $cB, $cR, $cL, &$outputArray){
 echo "<br>Start: <br>";
 for($i = 0; $i < $sideLength; $i++){
 	for($j = 0; $j < $sideLength; $j++){
-		printf("%02s\n", $outputArray[$i][$j]);
+		printf("%02s\n", $outputArray[$i][$j]); // This limits the number of digits to 2 and outputs the value of the element.
 	}
 	echo "<br>";
 }
